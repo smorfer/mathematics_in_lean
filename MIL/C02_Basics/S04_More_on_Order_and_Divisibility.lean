4db02ec08124f1ccd526420cd61e227f3555c38f
Samuel Glogger <smorfi98@gmail.com>
2025-06-04 02:37:31 +0200

    Sync progress

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -69,16 +69,36 @@ example : min (min a b) c = min a (min b c) := by
         . exact (h₀ _ _ _ (min_le_left _ _))
       . exact (h₀ _ _ _ (min_le_right _ _))
 
-
+#check add_neg_cancel_right
 
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
-example : min a b + c = min (a + c) (b + c) := by
-  sorry
-#check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
+  apply le_min
+  . apply add_le_add_right
+    . apply min_le_left
+  . apply add_le_add_right
+    . apply min_le_right
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+#check aux (a + c) (b + c) (-c)
+
+example : min a b + c = min (a + c) (b + c) := by
+  apply le_antisymm
+  . exact aux _ _ _
+  nth_rw 2 [← add_neg_cancel_right a c, ← add_neg_cancel_right b c]
+  apply sub_le_iff_le_add.mp
+  exact aux (a + c) (b + c) (-c)
+
+
+
+#check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
+#check sub_add_cancel
+#check add_neg_cancel_right
+
+
+example : |a| - |b| ≤ |a - b| := by
+  apply sub_le_iff_le_add.mpr
+  nth_rw 1 [← sub_add_cancel a b]
+  exact abs_add _ _
+
 end
 
 section
@@ -95,7 +115,14 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  . apply dvd_add
+    . apply dvd_mul_of_dvd_right
+      apply dvd_mul_right
+    . apply dvd_mul_left
+  . apply dvd_trans
+    . exact h
+    . apply dvd_mul_left
 end
 
 section
@@ -107,5 +134,8 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
-end
+  apply Nat.dvd_antisymm
+  repeat
+  . apply Nat.dvd_gcd
+    . apply Nat.gcd_dvd_right
+    . apply Nat.gcd_dvd_left
--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -36,16 +36,50 @@ variable (x y z : α)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
 example : x ⊓ y = y ⊓ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply le_inf
+    . apply inf_le_right
+    . apply inf_le_left
+
 
 example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
-  sorry
+  apply le_antisymm
+  . apply le_inf
+    . apply le_trans
+      . apply inf_le_left
+      . exact inf_le_left
+    . apply le_inf
+      . apply le_trans
+        . apply inf_le_left
+        . apply inf_le_right
+      . apply inf_le_right
+  . apply le_inf
+    . apply le_inf
+      . apply inf_le_left
+      . apply le_trans
+        . apply inf_le_right
+        . apply inf_le_left
+    . apply le_trans
+      . apply inf_le_right
+      . apply inf_le_right
+
 
 example : x ⊔ y = y ⊔ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply sup_le
+    . apply le_sup_right
+    . apply le_sup_left
+
 
 example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
-  sorry
+  apply le_antisymm
+  . apply sup_le
+    . apply sup_le
+      . apply le_sup_left
+      . apply le_trans
+        . apply le_sup_left
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
   sorry
@@ -109,4 +143,3 @@ example (x y : X) : 0 ≤ dist x y := by
   sorry
 
 end
-
